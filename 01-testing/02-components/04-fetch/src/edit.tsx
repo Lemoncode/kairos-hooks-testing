76ea13cea6906c8043ff13deb5b43248b46196be
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-04-03 11:08:34 +0200

    copy previous

---
+++ 01-testing/02-components/04-fetch/src/edit.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+  userName: string;
+  onSetUserName: (userName: string) => void;
+}
+
+export const Edit: React.FC<Props> = (props) => {
+  const { userName, onSetUserName } = props;
+
+  return (
+    <input value={userName} onChange={(e) => onSetUserName(e.target.value)} />
+  );
+};
