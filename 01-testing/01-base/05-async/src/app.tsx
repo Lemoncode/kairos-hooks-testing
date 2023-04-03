890536bbb90355e8f71f2b8502d732eebf390c51
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-04-03 10:50:00 +0200

    implement 05

--- 01-testing/01-base/05-async/src/app.tsx
+++ 01-testing/01-base/05-async/src/app.tsx
@@ -3,9 +3,11 @@ import { getMembers } from './api';
 
 export const App: React.FunctionComponent = () => {
   React.useEffect(() => {
-    getMembers().then((members) => {
-      console.log(members);
-    });
+    getMembers()
+      .then((members) => {
+        console.log(members);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   return <h1>05-Testing / 01 React</h1>;
