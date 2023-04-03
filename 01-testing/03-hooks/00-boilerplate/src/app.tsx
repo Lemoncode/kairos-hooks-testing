33e7bd0cb8da9de4a779d0b928dd189a0a0db166
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-04-03 11:37:04 +0200

    add boiler

--- 01-testing/03-hooks/00-boilerplate/src/app.tsx
+++ 01-testing/03-hooks/00-boilerplate/src/app.tsx
@@ -1,17 +1,5 @@
-import * as React from 'react';
-import { Router } from './router';
-import { CookiesDialog } from './cookies-dialog';
+import React from 'react';
 
-export const App: React.FunctionComponent = () => {
-  return (
-    <>
-      <h1>05-Testing / 01 React</h1>
-      <Router />
-      <CookiesDialog
-        onAgreeClick={() => {
-          console.log('Click agree');
-        }}
-      />
-    </>
-  );
+export const App: React.FC = () => {
+  return <h1>05-Testing / 01 React</h1>;
 };
