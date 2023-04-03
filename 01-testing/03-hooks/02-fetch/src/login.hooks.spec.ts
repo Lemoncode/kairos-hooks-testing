d6bbc895f49104aaf26d18deafe06f7507c7e7f4
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-04-03 11:58:07 +0200

    implement 02

--- 01-testing/03-hooks/02-fetch/src/login.hooks.spec.ts
+++ 01-testing/03-hooks/02-fetch/src/login.hooks.spec.ts
@@ -1,5 +1,6 @@
-import { renderHook, act } from '@testing-library/react';
-import { Credential } from './model';
+import { renderHook, act, waitFor } from '@testing-library/react';
+import * as api from './api';
+import { Credential, User } from './model';
 import { useLogin } from './login.hooks';
 
 describe('useLogin specs', () => {
@@ -28,5 +29,35 @@ describe('useLogin specs', () => {
 
     // Assert
     expect(result.current.credential).toEqual(newCredential);
+  });
+
+  it('should return user equals null and onLogin function', () => {
+    // Arrange
+
+    // Act
+    const { result } = renderHook(() => useLogin());
+
+    // Assert
+    expect(result.current.user).toBeNull();
+    expect(result.current.onLogin).toEqual(expect.any(Function));
+  });
+
+  it('should update user when it send valid credentials using onLogin', async () => {
+    // Arrange
+    const adminUser: User = { email: 'admin@email.com', role: 'admin' };
+    const loginStub = jest.spyOn(api, 'login').mockResolvedValue(adminUser);
+
+    // Act
+    const { result } = renderHook(() => useLogin());
+
+    act(() => {
+      result.current.onLogin();
+    });
+
+    // Assert
+    expect(loginStub).toHaveBeenCalled();
+    await waitFor(() => {
+      expect(result.current.user).toEqual(adminUser);
+    });
   });
 });
--- 01-testing/03-hooks/02-fetch/src/model.ts
+++ 01-testing/03-hooks/02-fetch/src/model.ts
@@ -2,3 +2,8 @@ export interface Credential {
   name: string;
   password: string;
 }
+
+export interface User {
+  email: string;
+  role: string;
+}
