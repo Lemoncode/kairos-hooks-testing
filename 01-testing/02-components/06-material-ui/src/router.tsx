9c5aad494891f7965538fbb85248dd0210e4577d
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-04-03 11:28:37 +0200

    copy previous

---
+++ 01-testing/02-components/06-material-ui/src/router.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { NameCollection } from './name-collection';
+import { UserEdit } from './user-edit';
+
+export const Router: React.FC = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<NameCollection />} />
+        <Route path="users/:name" element={<UserEdit />} />
+      </Routes>
+    </HashRouter>
+  );
+};
