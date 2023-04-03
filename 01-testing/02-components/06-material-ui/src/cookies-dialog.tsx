0248216bce4d9eb991239f23503e6e4fccc405c5
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-04-03 11:34:36 +0200

    implement 06

---
+++ 01-testing/02-components/06-material-ui/src/cookies-dialog.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
+
+interface Props {
+  onAgreeClick: () => void;
+}
+
+export const CookiesDialog: React.FC<Props> = (props) => {
+  const { onAgreeClick } = props;
+  const [open, setOpen] = React.useState(false);
+
+  const handleAgreeClick = () => {
+    setOpen(false);
+    onAgreeClick();
+  };
+
+  return (
+    <>
+      <Button variant="outlined" onClick={() => setOpen(true)}>
+        Learn more about our cookies
+      </Button>
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>About cookies</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Any information that you voluntarily provide to us, including your
+            name and email address, will be used for the sole purpose for which
+            the information was provided to us. In addition, communication
+            exchanges on this website are public (not private) communications.
+            Therefore, any message that you post on this website will be
+            considered and treated as available for public use and distribution.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button color="primary" onClick={handleAgreeClick}>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
